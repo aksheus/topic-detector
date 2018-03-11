1d686f8a79086ba4a7fff48c284a273162d9c27f
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2018-03-11 12:15:21 -0500

    considering verbs + adjectives

--- get_top10.py
+++ get_top10.py
@@ -2,26 +2,26 @@ import argparse
 import os
 import sys 
 import gc
-from nltk import word_tokenize,pos_tag
+from nltk import RegexpTokenizer,pos_tag
 from collections import defaultdict
 from operator import itemgetter
 from multiprocessing import Pool
-
 """
 
-       USAGE: python get_top10.py --path <path to directory containing .tsv files> --out <outfile> --stopw <path to stopwords file> [OPTIONAL] --top <top k topics default 10> 
+       USAGE: python get_top10.py --mode <task1 or task2> --path <path to directory or file> --out <out file name> --stopw <path to stopwords file> [OPTIONAL] --top <top k topics default 10> 
 
-       Example: python get_top10.py --path ./4  --out  ./top10from4.txt
+       Example1: python get_top10.py --mode task1 --path ./ubuntu_dialogs/4  --out  ./top10from4.txt --stopw stopwords.txt
 
-       Returns: top 10 topics in the corpus
+       Example2: python get_top10.py --mode task2 --path ./ubuntu_dialogs/4/3.tsv  --out  ./top10from4.txt --stopw stopwords.txt --top 5
+
+       Returns: top 10 topics in the corpus by default (or) top k topics 
 """
 
 def get_filenames(tsv_dir):
     """
          Args: directory containing corpus
 
-         Returns: files ireratively
-
+         Returns: files in directory ireratively
     """
     for tsv_filename in os.listdir(tsv_dir):
         yield os.path.join(tsv_dir,tsv_filename) 
@@ -33,12 +33,13 @@ def get_topics_from_file(tsv_file):
 
         Returns: candidate topics from file
     """
-    nouns = {'NN', 'NNS','NNPS'}
+    nouns = {'NN', 'NNS','NNPS','FW','JJ','JJR','JJS','VB','VBD','VBG','VBN','VBP','VBZ'}
     topics = []
+    tokenizer = RegexpTokenizer(r'\w+')
     with open(tsv_file,'r',errors='ignore') as tsvin:
         for row in tsvin:
             text = row.split('\t')[-1].strip()
-            tokens = word_tokenize(text)
+            tokens = tokenizer.tokenize(text)
             topics += [token.lower() for token,tag in pos_tag(tokens) if tag in nouns]
     return tuple(topics)
 
@@ -57,9 +58,9 @@ def get_stop_words(stop_words_file):
 
 
 
-def get_top10(tsv_dir,out_file,stop_words_file,k=10):
+def get_top10(mode,tsv_path,out_file,stop_words_file,k=10):
     """
-         Args: path to coprus, output file name, path to stop words file, return top k topics
+         Args: mode task1 (or) task2, path to coprus or file, output file name, path to stop words file, return top k topics
 
          Returns: top k topics in out file, by default 10  
 
@@ -68,10 +69,15 @@ def get_top10(tsv_dir,out_file,stop_words_file,k=10):
     pool = Pool()
     vocab = defaultdict(lambda:0,{})
 
-    all_topics = pool.imap(get_topics_from_file,get_filenames(tsv_dir))
-
-    for topics in all_topics:
-        for topic in topics:
+    if mode == 'task1':
+        all_topics = pool.imap(get_topics_from_file,get_filenames(tsv_path))
+        for topics in all_topics:
+            for topic in topics:
+                if topic not in stop_words:
+                    vocab[topic]+=1
+    elif mode=='task2':
+        all_topics = get_topics_from_file(tsv_path)
+        for topic in all_topics:
             if topic not in stop_words:
                 vocab[topic]+=1
 
@@ -85,15 +91,21 @@ def get_top10(tsv_dir,out_file,stop_words_file,k=10):
         out.write('\n')
         #out.write('Index Topic Occurences')
         out.write('\n')
-        for x in range(k):
-            out.write('{}) {} '.format(x+1,topics_freq[x][0]))
-            out.write('\n')
-    
+        if len(topics_freq) > k:
+            for x in range(k):
+                out.write('{}) {} '.format(x+1,topics_freq[x][0]))
+                out.write('\n')
+        else:
+            for x in range(len(topics_freq)):
+                out.write('{}) {} '.format(x+1,topics_freq[x][0]))
+                out.write('\n')
+        
 
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='python get_top10.py --path <path to directory containing .tsv files> --out <outfile> --stopw <path to stopwords file> [OPTIONAL] --top <top k topics default 10')
+    parser.add_argument('-m','--mode',help='task1 or task2',required=True)
     parser.add_argument('-p','--path',help='path to directory containing .tsv files',required=True)
     parser.add_argument('-o','--out',help='output file name',required=True)
     parser.add_argument('-s','--stopw',help='path to stopwords file',required=True)
@@ -101,14 +113,15 @@ if __name__ == '__main__':
 
     args= vars(parser.parse_args())
 
-    if os.path.isdir(args['path']):
+    if os.path.exists(args['path']):
         if args['top']!= None:
-            get_top10(args['path'],args['out'],args['stopw'],int(args['top']))
+            get_top10(args['mode'],args['path'],args['out'],args['stopw'],int(args['top']))
         else:
-            get_top10(args['path'],args['out'],args['stopw'])
+            get_top10(args['mode'],args['path'],args['out'],args['stopw'])
     else:
-        print('invalid directory for tsv file corpus, exiting with code 1')
+        print('invalid argument for path, exiting with code 1')
         sys.exit(1)
+
 
 
 
